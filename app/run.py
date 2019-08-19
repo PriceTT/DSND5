3abe887d7f7ac2d946e0d1f35bcb51edfe5f7cdb
jason <tt.price@gamil.com>
2019-08-19 13:17:56 +0000

    change saved model name to align with udacity template

--- app/run.py
+++ app/run.py
@@ -48,7 +48,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/disaster_model_Ad_Mod.sav")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
